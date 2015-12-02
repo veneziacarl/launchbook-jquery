7dbe6ae06e2ef01eca6eefdc23fe6374cfe06412
Carl Venezia <veneziacarl@gmail.com>
2015-12-02 14:46:33 -0500

    initial commit

---
+++ solutions.js
@@ -0,0 +1,31 @@
+// Exercise: Find the top navbar by query for the element type, which is <nav>.
+
+// Exercise: Find the sidebar on the right by using it's id.
+
+// Exercise: Find the "Pages" and "Groups" sections of the sidebar by using their class.
+
+// Exercise: Find all of the comments on the page.
+
+// Exercise: Find the first comment on the page.
+
+// Exercise: Find the last comment on the page.
+
+// Exercise: Find the fourth comment on the page.
+
+// Exercise: Find one of the ads in the sidebar and use .hide() to hide it.
+
+// Exercise: Use .show() to make the ad that you hid in the previous step visible again.
+
+// Exercise: Use .toggle() to toggle the display of the nav bar at the top of the page.
+
+// Exercise: Use .attr() to change src attribute of one of the ads in the sidebar. Here's an image source if you need one: http://placebear.com/200/300.
+
+// Exercise: Find Sam's post and change it's text to something incredible.
+
+// Exercise: Find the first post and use .addClass() to add the .post-liked class to it.
+
+// Exercise: Find the second post and use .removeClass() to remove the .post-liked class.
+
+// Exercise: Find any post and use .toggleClass() to toggle the .post-liked class.
+
+
